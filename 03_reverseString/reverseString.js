dcde3482a76b019a130418e95a75c694b21880ef
hackerover90 <isaac_noori@outlook.com>
2023-08-24 16:38:30 -0400

    Solve question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse_word = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverse_word += word[i];
+    }
+    return reverse_word
 };
 
 // Do not edit below this line
