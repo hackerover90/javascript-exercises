53561e0e558205d9223f46f736ae8057f4b4d919
hackerover90 <isaac_noori@outlook.com>
2023-09-23 15:18:43 -0400

    Complete exercise to learn use of regex in javascript

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+filteredStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+return filteredStr.split('').reverse().join('') == filteredStr;
 };
 
 // Do not edit below this line
