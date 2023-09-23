68303df2be0364846d57e3176d9e263177b6fcaa
hackerover90 <isaac_noori@outlook.com>
2023-09-23 15:51:23 -0400

    Demonstrate knowledge of object manipulation within arrays

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+let newArr = [];
+arr.forEach(obj => {
+    newArr.push(obj.title);
+});
+return newArr;
 };
 
 // Do not edit below this line
