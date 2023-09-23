2b9bae1f2622137723b9a08bfb94e7fd6af5e368
hackerover90 <isaac_noori@outlook.com>
2023-09-23 19:41:25 -0400

    Find oldest age in array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(arr) {
+
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = findAge(oldest);
+        const currentAge = findAge(currentPerson);
+        return oldestAge > currentAge ? oldest : currentPerson;
+    });
+
+    function findAge(element) {
+        if (!element.yearOfDeath) {
+            let today = new Date();
+            let year = today.getFullYear();
+            return year - element.yearOfBirth;
+        } else {
+            return element.yearOfDeath - element.yearOfBirth;
+        }
+    }
 };
 
 // Do not edit below this line
