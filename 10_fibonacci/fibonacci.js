2ec66753073ad3bc1dd7fff150fdeab72ac1eb89
hackerover90 <isaac_noori@outlook.com>
2023-09-23 15:42:39 -0400

    Solve fibanacci sequence using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    if (+fibNum == 1 || +fibNum == 0) {
+        return +fibNum;
+    } else if (fibNum < 0) {
+        return 'OOPS';
+    } else {
+        return fibonacci(+fibNum-1) + fibonacci(+fibNum-2);
+    }
 
+    
 };
 
 // Do not edit below this line
